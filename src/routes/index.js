d87b7d0153d8e0bba42287818c327e0ac9aae869
djr2323 <101247795+djr2323@users.noreply.github.com>
2023-04-28 08:40:55 +0700

    course detail page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const newsRouter = require("./news");
 const siteRouter = require("./site");
+const coursesRouter = require("./course");
 
 route = (app) => {
+  app.use("/courses",coursesRouter);
   app.use("/news", newsRouter);
   app.use("/", siteRouter);
 };
